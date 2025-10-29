ee9ffa274de0815c903a52da4d4884e9471ab75b
Leonel Gasparrini <jlgasparrini@gmail.com>
2025-10-29 04:02:33 -0300

    Rubocop offenses fixed

--- db/migrate/20251029065742_add_borrowings_count_to_books.rb
+++ db/migrate/20251029065742_add_borrowings_count_to_books.rb
@@ -1,7 +1,7 @@
 class AddBorrowingsCountToBooks < ActiveRecord::Migration[8.0]
   def change
     add_column :books, :borrowings_count, :integer, default: 0, null: false
-    
+
     # Backfill existing counts
     reversible do |dir|
       dir.up do
