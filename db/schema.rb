f6fe7c555fd528d0be29e983902e050f489c64ff
Leonel Gasparrini <jlgasparrini@gmail.com>
2025-10-29 03:33:31 -0300

    Index performance improvement

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_29_062301) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_29_063132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+  enable_extension "pg_trgm"
 
   create_table "books", force: :cascade do |t|
     t.string "title", null: false
@@ -23,10 +24,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_29_062301) do
     t.integer "available_copies", default: 1, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author"], name: "index_books_on_author"
+    t.index ["author"], name: "index_books_on_author_gin", opclass: :gin_trgm_ops, using: :gin
+    t.index ["available_copies"], name: "index_books_on_available_copies"
     t.index ["genre"], name: "index_books_on_genre"
     t.index ["isbn"], name: "index_books_on_isbn", unique: true
-    t.index ["title"], name: "index_books_on_title"
+    t.index ["title"], name: "index_books_on_title_gin", opclass: :gin_trgm_ops, using: :gin
   end
 
   create_table "jwt_denylist", force: :cascade do |t|
@@ -34,6 +36,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_29_062301) do
     t.datetime "exp", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["exp"], name: "index_jwt_denylist_on_exp"
     t.index ["jti"], name: "index_jwt_denylist_on_jti"
   end
 
@@ -48,5 +51,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_29_062301) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role"], name: "index_users_on_role"
   end
 end
